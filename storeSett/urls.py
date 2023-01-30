1da9903a05c2e9622e8030aa9da28bbd37b313ac
radhey <rinkeloin@gmail.com>
2023-01-30 15:07:06 +0530

    full vercel

--- storeSett/urls.py
+++ storeSett/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('eAuth/', include('eAuth.urls')),
     path('', include('storeApp.urls')),
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
