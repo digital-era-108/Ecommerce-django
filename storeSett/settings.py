46cef1d9a26de579f40e756ba76311475115436b
radhey <rinkeloin@gmail.com>
2023-01-30 14:04:01 +0530

    vercel

--- storeSett/settings.py
+++ storeSett/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7pkt+@1&+73s(w8@hoif8$9nw5&-d=cj_2i&s01-zc7!95d58c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
